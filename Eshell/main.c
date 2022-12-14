157967322624d98bd38eaff52c9b88f664347eab
Astro175 <aspark400@gmail.com>
2022-12-14 04:53:46 +0100

    working on tokenisation

--- Eshell/main.c
+++ Eshell/main.c
@@ -8,9 +8,13 @@
 
 int main(int ac, char **argv)
 {
-	char *prompt = "(Eshell) $ ";
-	char *lineptr;
-	size_t n = 0; 
+	char *prompt = " $ ";
+	char *lineptr, *lineptr_copy;
+    const char *delim = " \n"
+	size_t n = 0;
+    ssize_t nchars_read;
+    int num_tokens = 0;
+    char *token;    
 
 		        /* declaring void variables */
 	(void)ac; (void)argv;
@@ -19,13 +23,36 @@ int main(int ac, char **argv)
 	while (1)
 	{
 		printf("%s", prompt);
-		getline(&lineptr, &n, stdin);
-		printf("%s\n", lineptr);
-
-	/* free up allocated memory */
-		free(lineptr);
-	}
+		nchars_read = getline(&lineptr, &n, stdin);
+        if (nchars_read == -1)
+        {
+            printf("Exiting the shell...\n");
+            return (-1);
+        }
+        lineptr_copy = malloc(sizeof(char) * nchars);
+        if (lineptr_copy == NULL)
+        {
+            perror("memory allocation error");
+            return (-1);
+        }
+        strcpy(lineptr_copy, lineptr);
+        /* Splits strings into words */
+        token = strtok(lineptr, delim);
+        /* Determining how many tokens are there */
+        while(token != NULL)
+        {
+            num_tokens++;
+            token = strtok(NULL, delim);
+        }
+        num_tokens++;
+        /* Allocating space for the array of strings */
+        argv = malloc(sizeof(char) * num_tokens);
+		printf("%s", lineptr);
+        
 	
+	}
+	/* Frees up allocated space */
+    free(lineptr);
 	return (0);
 	}
 
