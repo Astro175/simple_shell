be2e9d61199367c7250277b97dcaa77cb9435dd3
Astro175 <aspark400@gmail.com>
2022-12-16 07:58:23 +0100

    New additions to get_location

--- Eshell/get_location.c
+++ Eshell/get_location.c
@@ -8,11 +8,23 @@
 
 char *get_location(char *command)
 {
-    char *path, *path_copy, *path_token;
+    char *path, *path_copy, *path_token, *file_path;
     path = getenv("PATH");
     path_copy = strdup(path);
     command_length = strlen(command);
+    int directory_length;
+
 
     path_token = strtok(path_copy, ":");
+    /**Allocate space for the command and directory length*/
+    directory_length = strlen(path_token);
+    file_path = malloc(command_length + directory_length + 2);
+    /* Copy result from path_token and append a forward slash and the command */
+    strcpy(file_path, path_token);
+    strcat(file_path, "/");
+    strcat(file_path, command);
+    strcat(file_path, "\0");
+
+
 
 }
